4fac47b4ca3aa964e673f34cd5d104ace55da57b
Isa Deu <ipd220@nyu.edu>
2018-02-04 16:37:28 -0500

    Open, game and end scene. ; movement OK, win state OK

---
+++ Assets/Scripts/mov.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mov : MonoBehaviour {
+
+	public float speed;
+	void Start () { 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			Vector3 targetTransform = Vector3.forward;
+			transform.position += targetTransform;
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			Vector3 targetTransform = Vector3.right;
+			transform.position += targetTransform;
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+			Vector3 targetTransform = Vector3.left;
+			transform.position += targetTransform;
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			Vector3 targetTransform = Vector3.back;
+			transform.position += targetTransform;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/win.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class win : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+		//rb = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+	void OnTriggerEnter (Collider c) {
+		if (c.gameObject.tag == "Player") {
+			print ("colliding with other");	
+		}
+	//	print ("oOOh");
+		//SceneManager.LoadScene ("win");
+	}
+}
