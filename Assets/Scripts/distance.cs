6b023a6a7ae834c3a2a144a6147a269731caa331
Isa Deu <ipd220@nyu.edu>
2018-02-04 21:08:47 -0500

    Player to object distance is recorded and when too far noticed

---
+++ Assets/Scripts/distance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class distance : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player) {
+			float dist = Vector3.Distance (player.position, transform.position);
+			print ("distance to other: " + dist);
+			if (dist >= 6f) {
+				print ("distance is too far 6");
+
+			
+			}
+		
+		}
+		
+	}
+}
