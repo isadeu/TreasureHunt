a954235aed9c4db92fc8186bbf5a2ddcb07ad0e3
Isa Deu <ipd220@nyu.edu>
2018-02-04 23:30:59 -0500

    realigned text, correct camera and object rot

--- Assets/Scripts/mov.cs
+++ Assets/Scripts/mov.cs
@@ -5,28 +5,38 @@ using UnityEngine;
 public class mov : MonoBehaviour {
 
 	public float speed;
-	void Start () { 
+	public GameObject player;
+	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.W)) {
-			Vector3 targetTransform = Vector3.forward;
-			transform.position += targetTransform;
+		/*if (Input.GetKeyDown (KeyCode.W)) {
+			Vector3 targetTransform = Vector3.forward; //new Vector3 (0, 0, 1, Space.Self);
+			player.transform.position += targetTransform;
 		}
 		if (Input.GetKeyDown (KeyCode.D)) {
 			Vector3 targetTransform = Vector3.right;
-			transform.position += targetTransform;
+			player.transform.position += targetTransform;
 		}
 		if (Input.GetKeyDown (KeyCode.A)) {
 			Vector3 targetTransform = Vector3.left;
-			transform.position += targetTransform;
+			player.transform.position += targetTransform;
 		}
 		if (Input.GetKeyDown (KeyCode.S)) {
 			Vector3 targetTransform = Vector3.back;
-			transform.position += targetTransform;
-		}
+			player.transform.position += targetTransform;
+		}*/
+
+
+		
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
 		
 	}
 }
--- Assets/Scripts/rotation.cs
+++ Assets/Scripts/rotation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class rotation : MonoBehaviour {
 
 
+	public GameObject player;
 	private Vector3 rotateval;
 	// Use this for initialization
 	void Start () {
@@ -13,15 +14,24 @@ public class rotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		/*
 		if (Input.GetKeyDown (KeyCode.A)) {
 			rotateval = new Vector3 (0, 90,0);
 			transform.eulerAngles = transform.eulerAngles - rotateval;
+			player.transform.Rotate (new Vector3 (0,-90,0));
 		}
 		if (Input.GetKeyDown (KeyCode.D)) {
-			rotateval = new Vector3 (0,-90,0);
-			transform.eulerAngles = transform.eulerAngles - rotateval;
+			//rotateval = new Vector3 (0,-90,0);
+			//transform.eulerAngles = transform.eulerAngles - rotateval;
+
+			player.transform.Rotate(0,90,0);
+			//player.transform.Rotate (new Vector3 (0,-90,0));
 		}
-		
+
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+
+		transform.Rotate(0, x, 0);
+	*/	
 	}
 }
