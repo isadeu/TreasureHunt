c56cb4de4f5593e2977350328d722002c325b34e
Isa Deu <ipd220@nyu.edu>
2018-02-04 21:49:48 -0500

    dk why the scripts didnt commit theyre important

---
+++ Assets/Scripts/distancethree.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class distancethree : MonoBehaviour {
+
+	public Transform player;
+	public Text clueTextThree;
+	//public bool texton;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (player) {
+			float dist = Vector3.Distance (player.position, transform.position);
+			print ("distance to other: " + dist);
+			if (dist >= 6f) {
+				//print ("distance is too far 6");
+				clueTextThree.text = "You are so far from the computer!";
+			}
+			if (dist < 6f) {
+				clueTextThree.text = " Nothing ";
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/distancetwo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class distancetwo : MonoBehaviour {
+
+	public Transform player;
+	public Text clueTextTwo;
+	//public bool texton;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (player) {
+			float dist = Vector3.Distance (player.position, transform.position);
+			print ("distance to other: " + dist);
+			if (dist >= 6f) {
+				//print ("distance is too far 6");
+				clueTextTwo.text = "You are too far from the tree";
+			}
+			if (dist < 6f) {
+				clueTextTwo.text = " notjing ";
+			}
+		}
+	}
+}
