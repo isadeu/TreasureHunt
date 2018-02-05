f171535f30ed8d62783dac2cc799965aa2d0aca0
Isa Deu <ipd220@nyu.edu>
2018-02-04 22:27:00 -0500

    camera rotation. thumbs up

---
+++ Assets/Scripts/rotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+
+
+	private Vector3 rotateval;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			rotateval = new Vector3 (0, 90,0);
+			transform.eulerAngles = transform.eulerAngles - rotateval;
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			rotateval = new Vector3 (0,-90,0);
+			transform.eulerAngles = transform.eulerAngles - rotateval;
+		}
+		
+	}
+}
